771a47878dc37a332c7a485eb9f6b432e6ffcbe8
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-02-22 00:10:26 +0500

    Redux Tool-Kit Todo Application

---
+++ 11_redux_Toolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,34 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{
+        id: nanoid(),
+        todoText: 'Hello to redux State Managment'
+    }
+        
+    ]
+}
+
+const todoSlice = createSlice({
+    name: "todos",
+    initialState: initialState,
+    reducers: {
+        addTodo: (state, actions)=>{
+            const todo = {
+                id: nanoid(),
+                todoText: actions.payload
+            }
+            state.todos.push(todo)
+        },
+        deleteTodo: (state, actions)=>{
+            state.todos.filter( (indiv_Todo)=> indiv_Todo.id !== actions.payload)
+        }
+
+    }
+})
+
+export const {addTodo, deleteTodo} = todoSlice.actions
+
+const todoReducer = todoSlice.reducer
+export default todoReducer
+
